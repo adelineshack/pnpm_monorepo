d5c4f294c75de767a159d8833c997a8228eecf0a
Adeline <faanfar@gmail.com>
2024-06-17 23:16:28 +0300

    init commit

---
+++ libs/src/index.ts
@@ -0,0 +1,11 @@
+export const giveMeTrue = () => {
+  return true;
+};
+
+export const giveMeDate = () => {
+  return new Date().getHours();
+};
+
+export const giveMewChance3 = () => {
+  return 'No';
+};
